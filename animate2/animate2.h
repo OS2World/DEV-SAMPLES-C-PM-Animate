4ea770564c47767a2a19341e4c4f6390213023fd
Martin Iturbide <martiniturbide@gmail.com>
2023-06-10 21:38:37 -0500

    1.01
    Compiled with gcc 9 under ArcaOS 5.0.7

---
+++ animate2/animate2.h
@@ -0,0 +1,11 @@
+/*------------------------
+   ANIMATE2.H header file
+  ------------------------*/
+
+#define ID_PROG          1
+#define IDM_EXIT       100
+#define IDM_RESUME     105
+
+#define ID_TIMER        10
+
+#define ID_FRAMES      800
